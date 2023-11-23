f544a8774b9510c1f88732c71697f3194a8ff3fd
Kian Shirvan <spoono17@gmail.com>
2023-11-22 23:33:27 -0700

    Initial file commit, changed command to be configurable

---
+++ client/client.lua
@@ -0,0 +1,46 @@
+local VORPcore = {}
+
+TriggerEvent("getCore", function(core)
+    VORPcore = core
+end)
+
+local isPvPEnabled = true
+
+function DisablePvPControls()
+    VORPcore.NotifyBottomRight(Config.notificationText.disabled, 4000)
+end
+
+function EnablePvPControls()
+    local message = Config.notificationText.enabled
+    VORPcore.NotifyBottomRight(message, 4000)
+    EnableControlAction(0, GetHashKey('INPUT_MELEE_ATTACK'), true)
+    EnableControlAction(0, GetHashKey('INPUT_MELEE_GRAPPLE_ATTACK'), true)
+    EnableControlAction(0, GetHashKey('INPUT_MELEE_GRAPPLE'), true)
+    EnableControlAction(0, GetHashKey('INPUT_MELEE_GRAPPLE_CHOKE'), true)
+end
+
+function TogglePvP()
+    SetResourceKvp(PlayerPedId(), "pvpEnabled")
+    isPvPEnabled = not isPvPEnabled
+    if not isPvPEnabled then
+        DisablePvPControls()
+    else
+        EnablePvPControls()
+    end
+end
+
+RegisterCommand(Config.Command, function()
+    TogglePvP()
+end, false)
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        if not isPvPEnabled then
+            DisableControlAction(0, GetHashKey('INPUT_MELEE_ATTACK'), true)
+            DisableControlAction(0, GetHashKey('INPUT_MELEE_GRAPPLE_ATTACK'), true)
+            DisableControlAction(0, GetHashKey('INPUT_MELEE_GRAPPLE'), true)
+            DisableControlAction(0, GetHashKey('INPUT_MELEE_GRAPPLE_CHOKE'), true)
+        end
+    end
+end)
---
+++ config.lua
@@ -0,0 +1,10 @@
+--DO NOT EDIT THIS PART--
+Config = {}
+
+Config.Command = 'pvp'
+
+--Notfication text for PvP toggle
+Config.notificationText = {
+    enabled = 'PvP is now enabled!',
+    disabled = 'PvP is now disabled!'
+}
